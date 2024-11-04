8724ce89d669552e1447f4a643f77f786f957b73
柯榛子 <197929057@qq.com>
2024-11-04 13:55:40 +0800

    优化结构 美化观感 添加翻页

--- Lesson1 Concentration/ContentView.swift
+++ Lesson1 Concentration/ContentView.swift
@@ -8,40 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["💀", "🎃", "👻", "😎", "👿"]
+    let emojis = ["💀", "🎃", "👻", "😎", "👿",
+                  "💀", "🎃", "👻", "😎", "👿",
+                  "💀", "🎃", "👻", "😎", "👿",
+                  "🎃", "👻", "😎", "👿"]
+    
+    @State var cardCount : Int = 4
+    
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) {
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    // 当 cardCount 小于 emojis 数组总数时，显示 "Add Card" 按钮
+    var cardAdder : some View {
+        return cardCountAdjusterBuilder(
+            by: +1, symbol: "rectangle.stack.fill.badge.plus"
+        )
+    }
+    
+    // 当 cardCount 大于 1 时，显示 "Remove Card" 按钮
+    var cardRemover : some View {
+        return cardCountAdjusterBuilder(
+            by: -1, symbol: "rectangle.stack.fill.badge.minus"
+        )
+    }
+    
+    // 卡片数组
+    var cards : some View {
+        // cards
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))], spacing: 10) {
+            ForEach(0..<cardCount, id: \.self) {
                 index in CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
         .imageScale(.small)
         .padding()
-        
+    }
+    
+    // 卡片调节器 +-
+    var cardCountAdjusters : some View {
+        HStack {
+            cardAdder
+            Spacer()
+            cardRemover
+        }
+        .padding()
+        .imageScale(.large)
+        .font(.title)
+    }
+    
+    // 构建 按扭
+    func cardCountAdjusterBuilder(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }) {
+            Image(systemName: symbol)
+        }
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
 }
-    
-struct CardView : View {
-    let content : String
-    @State var isFaceUp = true
+
+struct CardView: View {
+    let content: String
+    @State private var isFaceUp = true
     
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
+            
             if isFaceUp {
-                base.fill(.white)
+                // 正面内容
+                base.fill(Color.white)
                 base.strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [10, 2]))
                 Text(content).font(.largeTitle)
             } else {
-                base.fill()
+                // 背面内容
+                base.fill(Color.orange)
             }
         }
         .onTapGesture {
             isFaceUp.toggle()
         }
+        // 添加翻转动画
+        .animation(.easeInOut, value: isFaceUp)
     }
 }
+
 
 #Preview {
     ContentView()
